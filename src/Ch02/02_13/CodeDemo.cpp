9da3d84c9872eb2f2399abfc76aa68f6e2c82d1e
knguyencsc <knguyen.csc@gmail.com>
2026-08-06 23:07:24 +0000

    Update all the changes for CH02

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -9,6 +9,9 @@ int add_int(float a, double b, long double c){
     int result = 0;
 
     // Write your code here
+    result = result + static_cast<int>(a);
+    result = result + static_cast<int>(b);
+    result = result + static_cast<int>(c);
     
     return result;
 }
