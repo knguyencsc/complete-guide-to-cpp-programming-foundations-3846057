4366fb2e61a05f83fcd3cb5776cf380fada720a7
knguyencsc <knguyen.csc@gmail.com>
2026-09-22 21:35:03 +0000

    completed Chp06_02

--- src/Ch06/06_02b/CodeDemo.cpp
+++ src/Ch06/06_02b/CodeDemo.cpp
@@ -4,13 +4,35 @@
 
 #include <iostream>
 
+//Take arguments by value
+int square (int x){
+    return x*x;
+}
+
+// Take arguments by address
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+//Take arguments by reference
+void swap(int& x, int& y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     int a = 9, b;
     // TODO: square
+    b = square(a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap(&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     // TODO: swap
+    swap(a,b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
